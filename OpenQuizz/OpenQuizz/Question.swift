cc0610b7ca88b8fedbc610683e39ded283d88863
Élodie Gagé <elodiegage@gmail.com>
2022-07-27 12:00:17 +0200

    initial commit
    project remastered

---
+++ OpenQuizz/OpenQuizz/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by Elodie Gage on 27/07/2022
+//
+//
+
+import Foundation
+
+struct Question {
+    var title = ""
+    var isCorrect = false
+}
